209ab3a8b8bf5a96b3d76c4e51494ee5038d740c
netra3310 <netra0331@gmail.com>
2023-01-11 12:14:49 +0100

    0111 Whitelist dApp

--- 01_contract_compile_hardhat/scripts/deploy.js
+++ 01_contract_compile_hardhat/scripts/deploy.js
@@ -8,7 +8,7 @@ const hre = require("hardhat");
 
 function main() {
   // deployLock();
-  return deployNFTee();
+  return deployWhitelist();
 }
 
 async function deployLock() {
@@ -43,6 +43,24 @@ async function deployNFTee() {
 
   // print the address of the deployed contract
   console.log("NFT Contract Address:", deployedNFTContract.address);
+}
+
+async function deployWhitelist() {
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so whitelistContract here is a factory for instances of our Whitelist contract.
+  */
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
+
+  // here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // 10 is the Maximum number of whitelisted addresses allowed
+
+  // Wait for it to finish deploying
+  await deployedWhitelistContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("Whitelist Contract Address:", deployedWhitelistContract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
